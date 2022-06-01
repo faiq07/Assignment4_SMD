0ba414a88ed1ace70fcfc98456dc0b2d1d431f66
faiq07 <l191074@lhr.nu.edu.pk>
2022-06-02 03:01:08 +0500

    Assignment4

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,24 +1,69 @@
 package com.example.myapplication1
 
+import AirplaneModeChangeReceiver
 import android.content.Intent
+import android.content.IntentFilter
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //private var start: Button? = null
+    //private var stop: Button? = null
+    lateinit var receiver: AirplaneModeChangeReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        receiver = AirplaneModeChangeReceiver()
+        //start = findViewById<View>(R.id.startButton) as Button
+        //stop = findViewById<View>(R.id.stopButton) as Button
         val btn = findViewById<View>(R.id.button) as Button
+        var mp:MediaPlayer= MediaPlayer.create(this, R.raw.sample)
+
         btn.setOnClickListener()
         {
+
+            mp.start()
             val intent = Intent(this@MainActivity, FirstPage::class.java)
             startActivity(intent)
 
         }
 
 
+        //start!!.setOnClickListener(this)
+        //stop!!.setOnClickListener(this)
+
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
     }
+    /*override fun onClick(view: View) {
+
+        // process to be performed
+        // if start button is clicked
+        if (view === start) {
+
+            // starting the service
+            startService(Intent(this, servives::class.java))
+        }
+
+        // process to be performed
+        // if stop button is clicked
+        else if (view === stop) {
+
+            // stopping the service
+            stopService(Intent(this, servives::class.java))
+        }
+    }*/
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+
 }
--- app/src/main/java/com/example/myapplication1/SevenPage.kt
+++ app/src/main/java/com/example/myapplication1/SevenPage.kt
@@ -1,24 +1,64 @@
 package com.example.myapplication1
 
+import AirplaneModeChangeReceiver
 import android.content.Intent
+import android.content.IntentFilter
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class SevenPage: AppCompatActivity() {
+class SevenPage: AppCompatActivity() ,View.OnClickListener {
+
+    private var start: Button? = null
+    private var stop: Button? = null
+    lateinit var receiver: AirplaneModeChangeReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page7)
-
+        receiver = AirplaneModeChangeReceiver()
+        var mp: MediaPlayer = MediaPlayer.create(this, R.raw.sample)
         val btn = findViewById<View>(R.id.button7) as Button
+        start = findViewById<View>(R.id.startbutton) as Button
+        stop = findViewById<View>(R.id.Stopbutton) as Button
         btn.setOnClickListener()
         {
+            mp.start()
             val intent = Intent(this, EightPage::class.java)
             startActivity(intent)
 
         }
 
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            // registering the receiver
+            // it parameter which is passed in  registerReceiver() function
+            // is the intent filter that we have just created
+            registerReceiver(receiver, it)
+        }
+        start!!.setOnClickListener(this)
+        stop!!.setOnClickListener(this)
+    }
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
+    }
+    override fun onClick(view: View) {
 
+        // process to be performed
+        // if start button is clicked
+        if (view === start) {
+
+            // starting the service
+            startService(Intent(this, servives::class.java))
+        }
+
+        // process to be performed
+        // if stop button is clicked
+        else if (view === stop) {
+
+            // stopping the service
+            stopService(Intent(this, servives::class.java))
+        }
     }
 }
